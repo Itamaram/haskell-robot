e5058e6fff9c4ce8aea64d03a017e286d3018a28
Itamar Amith <itamaram@gmail.com>
2017-11-12 18:36:49 +1100

    Progress

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,82 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import Text.Printf
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Direction = N | E | S | W deriving Eq
+
+instance Show Direction where
+  show d = case d of
+    N -> "NORTH"
+    E -> "EAST"
+    S -> "SOUTH"
+    W -> "WEST"
+
+right' :: Direction -> Direction
+right' d = case d of
+  N -> E
+  E -> S
+  S -> W
+  w -> N
+
+left' :: Direction -> Direction
+left' d = case d of
+  N -> W
+  E -> N
+  S -> E
+  W -> S
+
+move' :: (Int, Int) -> Direction -> (Int, Int)
+move' (x, y) d = (x + dx, y + dy) where
+  (dx, dy) = case d of
+    N -> ( 0,  1)
+    E -> ( 1,  0)
+    S -> ( 0, -1)
+    W -> (-1,  0)
+
+onTheTable' :: Int -> Table -> Bool
+onTheTable' x (Table size) = 0 <= x && x < size
+
+onTheTable :: (Int, Int) -> Table -> Bool
+onTheTable (x, y) t = all (`onTheTable'` t) [x, y]
+
+data Robot = Robot {
+    x :: Int,
+    y :: Int,
+    direction :: Direction
+  }
+  deriving Eq
+
+instance Show Robot where
+  show (Robot x y d) = printf "%d, %d, %s" x y (show d)
+
+left :: Robot -> Robot
+left (Robot x y d) =
+  Robot x y (left' d)
+
+right :: Robot -> Robot
+right (Robot x y d) =
+  Robot x y (right' d)
+
+move :: Robot -> Robot
+move (Robot x y d) =
+  Robot x' y' d where (x', y') = move' (x, y) d
+
+data Command = Place Int Int Direction | Move | Left | Right | Report
+
+data Table = Table Int
+
+run :: Maybe Robot -> Table -> Command -> (Maybe Robot, Maybe String)
+run Nothing table (Place x y d) =
+  if onTheTable (x, y) table then (Just $ Robot x y d, Nothing) else (Nothing, Nothing)
+run Nothing _ _ = (Nothing, Nothing)
+run (Just (Robot x y d)) table command = 
+  case command of
+    Place x' y' d' -> if onTheTable (x', y') table then (Just $ Robot x' y' d', Nothing) else (Just $ Robot x y d, Nothing)
+    Move -> if onTheTable (x', y') table then (Just $ Robot x' y' d, Nothing) else (Just $ Robot x y d, Nothing)
+      where (x', y') = move' (x, y) d
+    Lib.Left -> (Just $ left (Robot x y d), Nothing)
+    Lib.Right-> (Just $ right (Robot x y d), Nothing)
+    Report -> (Just $ Robot x y d, Just $ show (Robot x y d))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,89 @@
+import Test.Hspec
+import Lib
+
+t5 = Table 5
+
+step :: (Maybe Robot, [String]) -> Command -> (Maybe Robot, [String]) 
+step (mr, ss) c = case ms of
+  Nothing -> (mr', ss)
+  Just s  -> (mr', ss ++ [s])
+  where (mr', ms) = run mr t5 c
+
+mockRun :: [Command] -> [String]
+mockRun cs = snd $ foldl step (Nothing, []) cs 
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "onTheTable'" $ do
+    it "returns true for EQ zero" $ do
+      onTheTable' 0 t5 `shouldBe` True
+    it "returns false for EQ bound" $ do
+      onTheTable' 5 t5 `shouldBe` False
+    it "returns true for in bound" $ do
+      onTheTable' 2 t5 `shouldBe` True
+    it "returns false for LT zero" $ do
+      onTheTable' (-1) t5 `shouldBe` False
+    it "returns false for GT bound" $ do
+      onTheTable' 6 t5 `shouldBe` False
+    
+  describe "onTheTable" $ do
+    it "returns false if x is off" $ do
+      onTheTable (-1, 0) t5 `shouldBe` False
+    it "returns false if y is off" $ do
+      onTheTable (0, -1) t5 `shouldBe` False
+    it "returns true if both are on the table" $ do
+      onTheTable (0, 0) t5 `shouldBe` True
+  
+  describe "move'" $ do
+    it "move N" $ do
+      move' (0, 0) N `shouldBe` (0, 1)
+    it "move S" $ do
+      move' (0, 0) S `shouldBe` (0, -1)
+    it "move W" $ do
+      move' (0, 0) W `shouldBe` (-1, 0)
+    it "move E" $ do
+      move' (0, 0) E `shouldBe` (1, 0)
+
+  describe "Place Command" $ do
+    it "place nothing on board" $ do
+      run Nothing t5 (Place 1 2 E) `shouldBe` (Just $ Robot 1 2 E, Nothing)
+    it "place existing on different spot" $ do
+      run (Just $ Robot 1 2 E) t5 (Place 3 4 N) `shouldBe` (Just $ Robot 3 4 N, Nothing)
+    it "place nothing off the table" $ do
+      run Nothing t5 (Place 0 5 N) `shouldBe` (Nothing, Nothing)
+    it "place existing off the table" $ do
+      run (Just $ Robot 1 2 E) t5 (Place 0 5 N) `shouldBe` (Just $ Robot 1 2 E, Nothing)
+    
+  describe "Move Command" $ do
+    it "doesn't place a robot on the board" $ do
+      run Nothing t5 Move `shouldBe` (Nothing, Nothing)
+    it "moves a robot on the board" $ do
+      run (Just $ Robot 0 0 N) t5 Move `shouldBe` (Just $ Robot 0 1 N, Nothing)
+    it "doesn't let a robot fall off" $ do
+      run (Just $ Robot 0 0 S) t5 Move `shouldBe` (Just $ Robot 0 0 S, Nothing)
+
+  describe "Left Command" $ do
+    it "doesn't place a robot on the board" $ do
+      run Nothing t5 Lib.Left `shouldBe` (Nothing, Nothing)
+    it "rotates a robot on the board to the left" $ do
+      run (Just $ Robot 0 0 N) t5 Lib.Left `shouldBe` (Just $ Robot 0 0 W, Nothing)
+
+  describe "Right Command" $ do
+    it "doesn't place a robot on the board" $ do
+      run Nothing t5 Lib.Right `shouldBe` (Nothing, Nothing)
+    it "rotates a robot on the board to the right" $ do
+      run (Just $ Robot 0 0 N) t5 Lib.Right `shouldBe` (Just $ Robot 0 0 E, Nothing)
+
+  describe "Report Command" $ do
+    it "doesn't report a robot off the board" $ do
+      run Nothing t5 Report `shouldBe` (Nothing, Nothing)
+    it "reports a robot on the board" $ do
+      run (Just $ Robot 1 2 E) t5 Report `shouldBe` (Just $ Robot 1 2 E, Just "1, 2, EAST")
+
+  describe "Examples" $ do
+    it "First" $ do
+      mockRun [Place 0 0 N, Move, Report] `shouldBe` ["0, 1, NORTH"]
+    it "Second" $ do
+      mockRun [Place 0 0 N, Lib.Left, Report] `shouldBe` ["0, 0, WEST"]
+    it "Third" $ do
+      mockRun [Place 1 2 E, Move, Move, Lib.Left, Move, Report] `shouldBe` ["3, 3, NORTH"]
